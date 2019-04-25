6632b258159ac76d523a7b892c45ac490a2cf6ef
ahaloulos <49919585+ahaloulos@users.noreply.github.com>
2019-04-25 13:19:57 -0500

    Update TreeScript.py

--- TreeScript.py
+++ TreeScript.py
@@ -1,1 +1,9 @@
 import arcpy
+from arcpy import env 
+env.workspace = "C://argis//treeproject"
+
+#Ask for inputs regarding pathogen spread 
+#Make list of sick target trees through data
+#Make multiring buffer around each ill tree in the list, will require another loop
+#Update sick tree list with target trees in buffer 
+#somehow have looping mechanism that will iterate through lines 7 and 8 until desired number of iterations is reached. 
